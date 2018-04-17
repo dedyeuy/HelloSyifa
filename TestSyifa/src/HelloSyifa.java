548d53a5b4a675cc6483a405d107b544227705d4
dedyeuy <dedyeuy@gmail.com>
2018-04-17 13:14:06 +0700

    second commit

--- TestSyifa/src/HelloSyifa.java
+++ TestSyifa/src/HelloSyifa.java
@@ -3,7 +3,7 @@ public class HelloSyifa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Syifa");
+System.out.println("Hello Syifa Azzahra Ramadhani, sedang test git commit dari eclipse");
 	}
 
 }
